598ba199bc6981eb7584f43b70e43980a4049407
emu <an.unidentified.emu@gmail.com>
2024-03-11 14:30:35 -0400

    1st round of textures for boss stage

--- levels/bits/texscroll.inc.c
+++ levels/bits/texscroll.inc.c
@@ -1,7 +1,7 @@
 void scroll_bits_dl_Cone_001_mesh_layer_1_vtx_0() {
 	int i = 0;
 	int count = 24;
-	int height = 64 * 0x20;
+	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
@@ -21,8 +21,8 @@ void scroll_bits_dl_Cone_001_mesh_layer_1_vtx_0() {
 
 void scroll_bits_dl_Cube_mesh_layer_1_vtx_0() {
 	int i = 0;
-	int count = 136;
-	int height = 64 * 0x20;
+	int count = 164;
+	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
@@ -40,10 +40,31 @@ void scroll_bits_dl_Cube_mesh_layer_1_vtx_0() {
 	currentY += deltaY;
 }
 
+void scroll_bits_dl_Cube_mesh_layer_1_vtx_1() {
+	int i = 0;
+	int count = 9;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(bits_dl_Cube_mesh_layer_1_vtx_1);
+
+	deltaY = (int)(0.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
 void scroll_bits_dl_Cube_001_mesh_layer_1_vtx_0() {
 	int i = 0;
 	int count = 69;
-	int height = 64 * 0x20;
+	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
@@ -64,7 +85,7 @@ void scroll_bits_dl_Cube_001_mesh_layer_1_vtx_0() {
 void scroll_bits_dl_Cone_mesh_layer_1_vtx_0() {
 	int i = 0;
 	int count = 40;
-	int height = 64 * 0x20;
+	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
@@ -85,7 +106,7 @@ void scroll_bits_dl_Cone_mesh_layer_1_vtx_0() {
 void scroll_bits_dl_Cube_003_mesh_layer_1_vtx_0() {
 	int i = 0;
 	int count = 124;
-	int height = 64 * 0x20;
+	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
@@ -106,7 +127,7 @@ void scroll_bits_dl_Cube_003_mesh_layer_1_vtx_0() {
 void scroll_bits_dl_Cube_003_mesh_layer_1_vtx_1() {
 	int i = 0;
 	int count = 30;
-	int height = 64 * 0x20;
+	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
@@ -127,6 +148,7 @@ void scroll_bits_dl_Cube_003_mesh_layer_1_vtx_1() {
 void scroll_bits() {
 	scroll_bits_dl_Cone_001_mesh_layer_1_vtx_0();
 	scroll_bits_dl_Cube_mesh_layer_1_vtx_0();
+	scroll_bits_dl_Cube_mesh_layer_1_vtx_1();
 	scroll_bits_dl_Cube_001_mesh_layer_1_vtx_0();
 	scroll_bits_dl_Cone_mesh_layer_1_vtx_0();
 	scroll_bits_dl_Cube_003_mesh_layer_1_vtx_0();
